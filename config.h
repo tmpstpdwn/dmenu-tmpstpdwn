b54dd863f8c2eb96c817a5cbf3e7847231e1595b
tmpstpdwn <tmpstpdwn@proton.me>
2025-04-29 11:36:29 +0530

    cleaned up config.h

--- config.h
+++ config.h
@@ -1,21 +1,27 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -b  option; if 0, dmenu appears at bottom */
+static int topbar = 1;
+
+/* -noi option; if 0, the input will not be drawn by default */
+static int draw_input = 1;                  
+
+ /* add an defined amount of pixels to the bar height */
+static const int user_bh = 5; 
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = { "SourceCodeVF:style=Semibold:size=10" };
 
-static int draw_input = 1;                  /* -noi option; if 0, the input will not be drawn by default */
+/* -p  option; prompt to the left of input field */
+static const char *prompt = NULL; 
 
-static const int user_bh = 5;               /* add an defined amount of pixels to the bar height */
-
-static const char *fonts[]          = { "SourceCodeVF:style=Semibold:size=10" };
-
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+/* color schemes */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+                 /*     fg         bg      */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel]  = { "#eeeeee", "#005577" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
