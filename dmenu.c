cf759386727d7618bada6e5ab1257f108d473ebf
tmpstpdwn <tmpstpdwn@proton.me>
2025-07-09 16:55:03 +0530

    Fixed illegal free's in dmenu.c, Fixed bugs in dmenu_launcher

--- dmenu.c
+++ dmenu.c
@@ -654,10 +654,6 @@ setup(void)
 	for (j = 0; j < SchemeLast; j++) {
 		scheme[j] = drw_scm_create(drw, (const char**)colors[j], 2);
 	}
-	for (j = 0; j < SchemeOut; ++j) {
-		for (i = 0; i < 2; ++i)
-			free(colors[j][i]);
-	}
 
 	clip = XInternAtom(dpy, "CLIPBOARD",   False);
 	utf8 = XInternAtom(dpy, "UTF8_STRING", False);
@@ -859,7 +855,6 @@ main(int argc, char *argv[])
 	if (!drw_fontset_create(drw, (const char**)fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 
-	free(fonts[0]);
 	lrpad = drw->fonts->h;
 
 #ifdef __OpenBSD__
